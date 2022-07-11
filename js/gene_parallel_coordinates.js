44019557d1378e3af8ea4f78e2bf00e53f18e29b
skvdev17 <skvdev17@cs.washington.edu>
2022-07-11 13:35:59 -0700

    Elegantly modified gene_table.js to internally filter based on whether the desired columns/rows match for the imodulons or the respective clusterings.

---
+++ js/gene_parallel_coordinates.js
@@ -0,0 +1,8 @@
+/**
+ * @summary Creates interactive parallel coordinate plot
+ * @author Vrishab Sathish Kumar
+ * requires d3.js
+ */
+
+// Plot the differential gene expression across columns (growth conditions)
+function generateParallelCoordsPlot(csvContent, container, organism, dataset)
